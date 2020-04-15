0c084ae3fd29572a5775d6664524fbdbe7dda137
fate <fatestylelove@outlook.com>
2020-04-15 17:54:25 +0800

    分类

--- app/controller/category.js
+++ app/controller/category.js
@@ -8,32 +8,9 @@ class CategoryController extends Controller {
      * 
      */
     async getCategory() {
-        console.log(11111111);
         const { ctx } = this;
-        const list = await ctx.service.category.find(13);
-        console.log(list);
-
-        ctx.status = 200;
-        ctx.status = 200;
-        ctx.status = 200;
-        ctx.status = 200;
-        ctx.status = 200;
-
-        ctx.set({
-            status: 200,
-        })
-
-        console.log(ctx.status);
-
-        console.log('-------');
-
-        return this.success({
-            id: 13,
-            name: 'IMSupperkaka',
-            account: 'IMSupperkaka',
-            password: 'IMSupperkaka',
-            created_at: '2020-01-27T03:46:44.000Z',
-            updated_at: '2020-01-27T03:46:44.000Z' });
+        const list = await ctx.service.category.list();
+        return this.success(list);
     }
 }
 
--- app/service/category.js
+++ app/service/category.js
@@ -1,19 +1,20 @@
 const Service = require('egg').Service;
 class CategoryService extends Service {
-  // 默认不需要提供构造函数。
-  // constructor(ctx) {
-  //   super(ctx); 如果需要在构造函数做一些处理，一定要有这句话，才能保证后面 `this.ctx`的使用。
-  //   // 就可以直接通过 this.ctx 获取 ctx 了
-  //   // 还可以直接通过 this.app 获取 app 了
-  // }
-  async find(uid) {
-    // 假如 我们拿到用户 id 从数据库获取用户详细信息
-    return this.app.mysql.query('select * from users where id = ?', [uid]);
-
-    // return {
-    //   name: user.user_name,
-    //   age: user.age,
-    // };
-  }
+    // 默认不需要提供构造函数。
+    // constructor(ctx) {
+    //   super(ctx); 如果需要在构造函数做一些处理，一定要有这句话，才能保证后面 `this.ctx`的使用。
+    //   // 就可以直接通过 this.ctx 获取 ctx 了
+    //   // 还可以直接通过 this.app 获取 app 了
+    // }
+    async list() {
+        // 假如 我们拿到用户 id 从数据库获取用户详细信息
+        return this.app.mysql.select('category', { // 搜索 post 表
+            where: { status: 1 }, // WHERE 条件
+            columns: ['name'], // 要查询的表字段
+            orders: [['sort', 'asc']], // 排序方式
+            // limit: 10, // 返回数据量
+            // offset: 0, // 数据偏移量
+        })
+    }
 }
 module.exports = CategoryService;
