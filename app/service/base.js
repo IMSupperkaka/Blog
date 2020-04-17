87245aec42fca87a32c10d5d1c58b604465d71a2
fate <fatestylelove@outlook.com>
2020-04-17 13:44:48 +0800

    分页

--- app/service/base.js
+++ app/service/base.js
@@ -1,10 +1,18 @@
 const Service = require('egg').Service;
 class BaseService extends Service {
     // 处理分页数据
-    async pageData(countSql, countField, sql, { offset, limit, page, pageSize }) {
+    async pageData(sql, { offset, limit, page: currentPage, pageSize }) {
+        // 统计字段
+        let countField = sql.match(/^select\s+(\S+?)[,| ]/i)[1];
+        console.log(countField);
+        // 需要替换的字符串
+        let needReplaceStr = sql.match(/^select\s(.*?)\sfrom/i)[1];
+        console.log(needReplaceStr);
+        // 统计总数的语句
+        let countSql = sql.replace(needReplaceStr, `count(${countField}) as nums`).replace(/limit.*?$/i, '');
         // 总条数
         let countData = await this.app.mysql.query(countSql);
-        let count = countData[0][countField]
+        let count = countData[0].nums;
         // 总页数
         let totalPages = Math.ceil(count / limit);
         // 列表数据
@@ -13,7 +21,7 @@ class BaseService extends Service {
             count,
             totalPages,
             pageSize,
-            currentPage: page,
+            currentPage,
             data: listData
         }
     }
